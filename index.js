74c74498ce69b77689784dd232c7603a2de6aa6b
Xiaoyi Liu <lxy878@gmail.com>
2020-09-08 04:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ class Deciduous extends Tree{
     return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
+
+class Evergreen extends Tree {
+  constructor() {
+    
+  }
+}
